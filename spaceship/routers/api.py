64bebb9ae16dbb8cddfdc8b9e119cc981424c290
Rembqq <nikitavolkovskyi@gmail.com>
2024-04-11 19:28:10 +0300

    matrix multiplication added

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,18 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/multiply_matrices')
+def multiply_matrices() -> dict:
+    # Generate two random matrices of size 10x10
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+
+    # Multiply the matrices
+    result_matrix = np.dot(matrix_a, matrix_b)
+
+    return {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "result_matrix": result_matrix.tolist()
+    }
